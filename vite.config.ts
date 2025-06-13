3127173af41e3548529c8c37dd2a6ead88cb07c4
Sokpin <157196803+PrenToCee@users.noreply.github.com>
2025-06-13 10:14:01 +0700

    root

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,4 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
-  root: "worker",
-  build: {
-    outDir: "../dist",
-  },
 });
